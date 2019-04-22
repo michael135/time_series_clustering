fecfe99cb89cb4325ce3ea585fb1234f3232bef8
Michael D <m.dmshts@gmail.com>
2019-04-22 10:11:56 +0300

    test features added

---
+++ test_featrue_values.R
@@ -0,0 +1,30 @@
+library("tsgeneration")
+library("tsfeatures")
+
+
+
+feature_class = c(f"max_var_shift", "max_kl_shift", "pacf_features", "stl_features")
+
+selected_features = c("max_var_shift", "max_kl_shift", "seas_pacf", "seasonal_strength")
+
+strong_features_params = c(77, 73, 0.01, 190)
+
+
+
+output <- generate_ts_with_target(n = 1000, ts.length = 300,
+								freq = 24, seasonal = 1,
+                                features = feature_class, 
+								selected.features = selected_features, 
+                                target = param_high)
+
+
+write.csv(x, file="test-generated.csv")
+
+# check the feature outputs 
+
+features_for_ts_features   <- c("length", "max_var_shift",
+				                "pacf_features", "stl_features", 
+				                "max_level_shift", "max_kl_shift")
+
+write.csv(featurelist, file="test-measurement.csv")
+
